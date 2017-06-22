a345035f64b77d3a90870c5eaa99f7b7c2b9f33a
Joel De Gan <joeldg@rxmg.com>
2017-06-22 16:54:08 -0700

    Docker fixes

--- docker/resources/config_bowhead.sh
+++ docker/resources/config_bowhead.sh
@@ -8,13 +8,16 @@ phpenmod trader
 phpenmod mcrypt
 service php7.1-fpm start
 service mysql start
+service redis-server start
 mysqladmin -u root password password
 echo "CREATE DATABASE bowhead;" | mysql -u root -ppassword
 
 cd ~
 git clone https://github.com/joeldg/bowhead.git
 cd bowhead
-echo "------ THIS IS GOING TO TAKE A LITTLE WHILE ..... please wait."
+echo "-----------------------------------------------------------------"
+echo "------ THIS IS GOING TO TAKE A LITTLE WHILE ..... please wait. --"
+echo "-----------------------------------------------------------------"
 composer update
 cp .env.example .env
 
@@ -28,10 +31,14 @@ php artisan bowhead:example_usage
 #/usr/bin/crontab /usr/src/crontab.tmp
 #/usr/sbin/service cron start
 
-echo "------ Bowhead is now set up                                        ------"
+echo "--------------------------------------------------------------------------"
+echo "------ Bowhead is now set up:                                       ------"
+echo "------ you need to modify your /root/bowhead/.env                   ------"
+echo "------                                                              ------"
 echo "------ SWAP TO A DIFFERENT TERMINAL TO CONNECT TO THIS INSTANCE     ------"
 echo "------ USE: 'docker ps' to see instance id (on the left)            ------"
 echo "------ USE: 'docker exec -it {id} /bin/bash to get terminal access  ------"
+echo "--------------------------------------------------------------------------"
 
 # fire up supervisord
 /usr/bin/supervisord
