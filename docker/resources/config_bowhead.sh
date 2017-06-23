5aa91facefad8c1b6f2afebe8dfadd7c3f94cc4d
Joel De Gan <joeldg@rxmg.com>
2017-06-22 21:48:42 -0700

    Dockerfile

--- docker/resources/config_bowhead.sh
+++ docker/resources/config_bowhead.sh
@@ -9,6 +9,12 @@ phpenmod mcrypt
 service php7.1-fpm start
 service mysql start
 service redis-server start
+adduser www-data root
+
+pushd /etc/nginx/sites-enabled
+ln -s ../sites-available/bowhead.conf .
+popd
+
 mysqladmin -u root password password
 echo "CREATE DATABASE bowhead;" | mysql -u root -ppassword
 
@@ -23,7 +29,7 @@ echo "-----------------------------------------------------------------"
 composer update
 cp .env.example .env
 
-ln -s /root/bowhead/public /var/www/html/bowhead
+ln -s /var/www/bowhead/public /var/www/html/bowhead
 
 mkfifo quotes
 mysql -u root -ppassword -D bowhead < app/Scripts/DBdump.sql
