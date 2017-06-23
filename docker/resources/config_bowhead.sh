6802878dc3ae52e6db04823ff625154419506fea
Joel De Gan <joeldg@rxmg.com>
2017-06-22 19:13:22 -0700

    Docker port changes

--- docker/resources/config_bowhead.sh
+++ docker/resources/config_bowhead.sh
@@ -15,6 +15,8 @@ echo "CREATE DATABASE bowhead;" | mysql -u root -ppassword
 cd ~
 git clone https://github.com/joeldg/bowhead.git
 cd bowhead
+pip install python-env
+
 echo "-----------------------------------------------------------------"
 echo "------ THIS IS GOING TO TAKE A LITTLE WHILE ..... please wait. --"
 echo "-----------------------------------------------------------------"
@@ -31,14 +33,21 @@ php artisan bowhead:example_usage
 #/usr/bin/crontab /usr/src/crontab.tmp
 #/usr/sbin/service cron start
 
-echo "--------------------------------------------------------------------------"
-echo "------ Bowhead is now set up:                                       ------"
-echo "------ you need to modify your /root/bowhead/.env                   ------"
-echo "------                                                              ------"
-echo "------ SWAP TO A DIFFERENT TERMINAL TO CONNECT TO THIS INSTANCE     ------"
-echo "------ USE: 'docker ps' to see instance id (on the left)            ------"
-echo "------ USE: 'docker exec -it {id} /bin/bash to get terminal access  ------"
-echo "--------------------------------------------------------------------------"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+----- READ ME:                                                     -----+"
+echo "+------------------------------------------------------------------------+"
+echo "+----- Bowhead is now set up:                                       -----+"
+echo "+----- you need to modify your /root/bowhead/.env                   -----+"
+echo "+-----                                                              -----+"
+echo "+----- SWAP TO A DIFFERENT TERMINAL TO CONNECT TO THIS INSTANCE     -----+"
+echo "+----- USE: 'docker ps' to see instance id (on the left)            -----+"
+echo "+----- USE: 'docker exec -it {id} /bin/bash to get terminal access  -----+"
+echo "+-----                                                              -----+"
+echo "+-----  oanda streaming is going to 'Fatal' exit until              -----+"
+echo "+-----  you set your OANDA_TOKEN in .env                            -----+"
+echo "+-----                                                              -----+"
+echo "+-----  use: 'php artisan bowhead:example_usage' for testing .env   -----+"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 # fire up supervisord
 /usr/bin/supervisord
