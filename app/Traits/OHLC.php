6067856bf2a7e1f8051a72c788db95fec3eaeec2
Andy Giles <andyg5000@gmail.com>
2017-11-08 09:49:32 -0500

    Ticker is already in the appropriate format for OHLC trait

--- app/Traits/OHLC.php
+++ app/Traits/OHLC.php
@@ -29,7 +29,6 @@ trait OHLC
             $timeid = ($ticker['timeid'] ?? $timeid);
         } else {
             /** Oanda websocket */
-            $ticker = json_decode($ticker, 1);
             $last_price = $ticker['tick']['bid'];
             $instrument = $ticker['tick']['instrument'];
             $volume = 0;
