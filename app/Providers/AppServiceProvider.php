63e2ac352d1f1f5c8459e9dbea09d35e4d423901
Joel De Gan <joeldg@rxmg.com>
2018-01-07 02:49:55 -0500

    oops

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace Bowhead\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
